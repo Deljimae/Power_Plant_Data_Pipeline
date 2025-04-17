96ab24f0120fcb03583655135d8d305fef07ef41
Ayodeji Lana <64302909+Deljimae@users.noreply.github.com>
2025-04-17 22:14:31 +0100

    Update plants_commissioned_by_year.sql

--- Athena_Transformation/plants_commissioned_by_year.sql
+++ Athena_Transformation/plants_commissioned_by_year.sql
@@ -1,4 +1,4 @@
-CREATE TABLE kestradb.plants_commissioned_by_year AS
+CREATE TABLE kestradb.plants_commissioned_by_year_recent AS
 SELECT 
     commissioning_year AS year,
     COUNT(*) AS plant_count
@@ -6,8 +6,8 @@ FROM
     kestradb.global_power_plants_iceberg
 WHERE 
     commissioning_year IS NOT NULL
+    AND commissioning_year >= 2000
 GROUP BY 
     commissioning_year
 ORDER BY 
     year ASC;
-
